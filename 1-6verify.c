e4b33a156fc7adfd8e4cf50e0c59f0fc7830717d
Abhigyan <singhdefinite12@gmail.com>
2025-07-01 10:51:27 +0530

    page 18

---
+++ 1-6verify.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    // int c ;
+    // if(getchar() != EOF){
+    //     printf("1");
+    // }
+    // else {
+    //     printf("0");
+    // }
+
+    int p = EOF ;
+    printf("%d ", p);
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+  // int c ;
+    // if(getchar() != EOF){
+    //     printf("1");
+    // }
+    // else {
+    //     printf("0");
+    // }
